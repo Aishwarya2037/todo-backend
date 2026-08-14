80d687ef8a325fbc9ea0e22bc35331965e29ac00
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-08-14 13:12:46 +0530

    fix render deployment

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,33 @@
 // });
 
 // while connecting mysql
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import app from "./app";
+// import db from "./db";
+
+// const PORT = Number(process.env.PORT) || 5000;
+
+// async function startServer() {
+//   try {
+//     const connection = await db.getConnection();
+
+//     console.log("MySQL connected successfully");
+
+//     connection.release();
+
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://localhost:${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("MySQL connection failed:", error);
+//     process.exit(1);
+//   }
+// }
+
+// startServer();
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -23,8 +50,8 @@ async function startServer() {
 
     connection.release();
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
     console.error("MySQL connection failed:", error);
